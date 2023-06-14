ece6b67070b885ec333652019503c5c547b88f31
brianlorenzo <isebbrianlorenzo@gmail.com>
2023-06-14 10:36:40 -0300

    organizacion de archivos

---
+++ videos/test/setupVideos.py
@@ -0,0 +1,8 @@
+# Crear un objeto que sea video que tenga información de
+# data  =   identificativo de dato escrito en tag
+# url   =   link de donde descargarlo si es que no está guardado
+# name  =   nombre del video para registro interno
+
+class videos:
+    def __init__(self, id_video):
+        self.id_video = id_video
---
+++ videos/test/videoPlayer.py
@@ -0,0 +1,26 @@
+import vlc
+
+# Ruta completa del archivo de video
+ruta_video = 'olimp2022(360p).mp4'
+
+# Crear instancia del reproductor VLC
+vlc_instance = vlc.Instance()
+
+# Crear reproductor
+reproductor = vlc_instance.media_player_new()
+
+# Cargar el archivo de video
+media = vlc_instance.media_new(ruta_video)
+
+# Establecer la media en el reproductor
+reproductor.set_media(media)
+
+# Reproducir el video
+reproductor.play()
+
+# Esperar a que la reproducción termine
+while reproductor.is_playing():
+    pass
+
+# Liberar recursos
+reproductor.stop()
